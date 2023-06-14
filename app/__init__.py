c38b2bd226b7b4c545f888a005ce3b02483689c7
Sean <seanpatmeade@gmail.com>
2023-06-15 00:40:31 +0100

    Set up for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,9 @@ def create_app():
     app = Flask(__name__)
 
     if os.environ.get('DEVELOPMENT'):
-       app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    else:
+        os.environ.get('DATABASE_URL')
 
     api.init_app(app)
     db.init_app(app)
